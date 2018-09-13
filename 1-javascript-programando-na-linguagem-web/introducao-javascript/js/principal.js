31724ff68434db92bf22d8484e2543c431279d14
Fernanda Aparecida <ferreira.ap.fernanda@gmail.com>
2018-09-12 21:54:36 -0300

    javascript aula 02

--- 1-javascript-programando-na-linguagem-web/introducao-javascript/js/principal.js
+++ 1-javascript-programando-na-linguagem-web/introducao-javascript/js/principal.js
@@ -1,2 +1,31 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+// Dados da página
+var paciente = document.querySelector("#primeiro-paciente");
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var tdImc = paciente.querySelector(".info-imc");
+
+// Validação
+var pesoValido = true;
+var alturaValida = true;
+
+var mensagem = "";
+
+if(peso <= 0 || peso > 1000){
+    pesoValido = false;
+    mensagem = "Peso inválido";
+}
+if(altura <= 0 || altura >= 3){
+    alturaValida = false;
+    mensagem = "Altura inválida";
+}
+
+if(pesoValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+    console.log(imc);
+}else{
+    tdImc.textContent = mensagem;
+}
