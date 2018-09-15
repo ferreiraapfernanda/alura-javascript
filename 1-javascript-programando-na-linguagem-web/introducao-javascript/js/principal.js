2dedfd7b06b14e8c4f46bf6a31fdefa9bc139782
Fernanda Aparecida <ferreira.ap.fernanda@gmail.com>
2018-09-14 21:29:19 -0300

    javascript 1 - aula 04

--- 1-javascript-programando-na-linguagem-web/introducao-javascript/js/principal.js
+++ 1-javascript-programando-na-linguagem-web/introducao-javascript/js/principal.js
@@ -2,30 +2,78 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 // Dados da página
-var paciente = document.querySelector("#primeiro-paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
+pacientes.forEach(calculaImc);
 
-// Validação
-var pesoValido = true;
-var alturaValida = true;
+function calculaImc(paciente, index, array) {
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var mensagem = "";
+    // Validação
+    var pesoValido = true;
+    var alturaValida = true;
 
-if(peso <= 0 || peso > 1000){
-    pesoValido = false;
-    mensagem = "Peso inválido";
+    var mensagem = "";
+
+    if (peso <= 0 || peso > 1000) {
+        pesoValido = false;
+        mensagem = "Peso inválido";
+    }
+    if (altura <= 0 || altura >= 3) {
+        alturaValida = false;
+        mensagem = "Altura inválida";
+    }
+
+    if (pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        imc = imc.toFixed(2);
+        tdImc.textContent = imc;
+    } else {
+        tdImc.textContent = mensagem;
+        paciente.classList.add("paciente-invalido");
+    }
 }
-if(altura <= 0 || altura >= 3){
-    alturaValida = false;
-    mensagem = "Altura inválida";
-}
 
-if(pesoValido && alturaValida){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-    console.log(imc);
-}else{
-    tdImc.textContent = mensagem;
-}
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Seleciona o formulário
+    var form = document.querySelector('#form-adiciona');
+
+    // Pega os dados do form
+    var nome = form.nome.value;
+    var peso = form.nome.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var novoPaciente = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    novoPaciente.classList.add("paciente");
+    nomeTd.classList.add("info-nome");
+    pesoTd.classList.add("info-peso");
+    alturaTd.classList.add("info-altura");
+    gorduraTd.classList.add("info-gordura");
+    imcTd.classList.add("info-imc");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    novoPaciente.appendChild(nomeTd);
+    novoPaciente.appendChild(pesoTd);
+    novoPaciente.appendChild(alturaTd);
+    novoPaciente.appendChild(gorduraTd);
+    novoPaciente.appendChild(imcTd);
+
+    document.querySelector("#tabela-pacientes").appendChild(novoPaciente);
+
+
+})
--- 1-javascript-programando-na-linguagem-web/readme.md
+++ 1-javascript-programando-na-linguagem-web/readme.md
@@ -1,8 +1,54 @@
 # JavaScript: Programando na linguagem da web
 
-```javascript
-var titulo = document.querySelector("h1");
-var titulo = document.querySelector(".titulo"); //ou
-var titulo = document.querySelector("#titulo"); //ou
-titulo.textContent = "Novo título";
-```
+- **Selecionando uma tag/classe/id na página**
+
+    ```javascript
+    var titulo = document.querySelector("h1");
+    var titulo = document.querySelector(".titulo"); //ou
+    var titulo = document.querySelector("#titulo"); //ou
+    titulo.textContent = "Novo título";
+    ```
+
+- **Selecionando todos os itens**
+
+    ```js
+    var todosOsTitulos = document.querySelectorAll(".titulo");
+
+    for(var i=0; i<pacientes.length;i++){
+        console.log(pacientes[i]);
+    }
+    ```
+
+- **Formatação de números e estilo**
+
+    ```js
+    num.toFixed(2); // com 2 casas decimais
+    num.style.color = "red"; // altera a propriedade do estilo
+    num.classList.add("numero-invalido");
+    ```
+- **Listener**
+
+    ```js
+    botao.addEventListener('click', funcaoDoBotao);
+
+    function funcaoDoBotao(event){
+        event.preventDefault();
+        console.log('Botao clicado');
+    }
+    ```
+- **Formulário**
+
+    Quando pegamos os dados dos inputs do form, podemos pegar direto pelo seu name.
+    ```js
+    var form = document.querySelector("#form");
+    var nome = form.nome.value;
+    ```
+- **Criando elemento**
+
+    ```js
+    var novoDado = document.createElement("td");
+    linhaTabela.appendChild(novoDado);
+    ```
+- **Event shortcut**
+
+    Para cada evento existente no JavaScript, há a propriedade on + nomeDoEvent. No caso, temos onclick para o evento click, onmouseover para o evento mouseover e assim por diante. **Não é recomendado seu uso**
